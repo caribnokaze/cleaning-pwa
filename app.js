24863a638e51d872e7dc275372083a119823dffe
caribnokaze <125463186+caribnokaze@users.noreply.github.com>
2026-01-13 22:51:33 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 async function send() {
-  alert("あとでGASとつなぎます");
+  const staff = document.getElementById("staff").value;
+  const site  = document.getElementById("site").value;
+  const files = document.getElementById("photos").files;
+
+  if (!files.length) {
+    alert("写真を選択してください");
+    return;
+  }
+
+  const formData = new FormData();
+  formData.append("staff", staff);
+  formData.append("site", site);
+
+  for (const file of files) {
+    formData.append("files", file);
+  }
+
+  try {
+    await fetch("https://script.google.com/macros/s/AKfycbyk5PoolRe9llCmKzPEIzLlaHpxNh673v_olguo-V6vE3ZoQUlLG7MKbgycpDg8o0ojaA/exec", {
+      method: "POST",
+      body: formData
+    });
+
+    alert("送信完了しました");
+  } catch (e) {
+    alert("送信に失敗しました");
+  }
 }
